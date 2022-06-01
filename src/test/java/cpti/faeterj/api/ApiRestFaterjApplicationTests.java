197e1868b11bf120a860bd2783a98510212722c6
Julia Neumann <julianeumannb@hotmail.com>
2022-06-01 19:51:50 -0300

    first commit

---
+++ src/test/java/cpti/faeterj/api/ApiRestFaterjApplicationTests.java
@@ -0,0 +1,13 @@
+package cpti.faeterj.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFaterjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
